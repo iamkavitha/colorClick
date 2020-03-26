22e340ed43c65a9ea98210e87e35d01afadf05e0
Kavitha <62335763+iamkavitha@users.noreply.github.com>
2020-03-26 11:34:30 +0530

    Added files to repository

---
+++ app.js
@@ -0,0 +1,19 @@
+function setColumnColor(column) {
+    column.style.backgroundColor="black"
+}
+function setColumnColor1(column1) {
+    column1.style.backgroundColor="green"
+}
+function setColumnColor2(column2) {
+    column2.style.backgroundColor="red"
+}
+function setColumnColor3(column3) {
+    column3.style.backgroundColor="purple"
+}
+function setColumnColor4(column4) {
+    column4.style.backgroundColor="yellow"
+}
+function setColumnColor5(column5) {
+    column5.style.backgroundColor="blue"
+}
+
